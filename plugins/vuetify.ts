7ded8e75c66bf18197fa85bb63d481aec87e8f97
root <halomrgn@gmail.com>
2025-03-02 13:12:41 +0000

    from vps

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -5,7 +5,7 @@ import { fa } from "vuetify/iconsets/fa";
 import { aliases, mdi } from "vuetify/lib/iconsets/mdi";
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
-
+import "vuetify/styles";
 
 import "@mdi/font/css/materialdesignicons.css";
 import "@fortawesome/fontawesome-free/css/all.css";
