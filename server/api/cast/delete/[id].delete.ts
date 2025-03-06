0528211dd5208badc8004262f7ba714107f0b701
M0R6 <tellmesomething.morgan@gmail.com>
2025-03-06 20:10:00 +0700

    merge (local)

--- server/api/cast/delete/[id].delete.ts
+++ server/api/cast/delete/[id].delete.ts
@@ -1,4 +1,4 @@
-import { exec } from 'child_process'
+import { unlink } from 'fs/promises';
 import { PrismaClient } from '@prisma/client'
 import fs from 'fs'
 import path from 'path'
@@ -27,12 +27,12 @@ export default defineEventHandler(async (event) => {
         const castPath = cast.photo ? path.join(process.cwd(), uploadDir, cast.photo) : null
 
         // Delete the files from the filesystem
-	if (castPath && fs.existsSync(castPath)) {
-            await fs.promises.unlink(castPath);
+        if (castPath) {
+            await unlink(castPath).catch(() => console.warn("Failed to delete old poster"));
             console.log("File deleted successfully!");
         } else {
             console.log("File not found or already deleted:", castPath);
-        }	
+        }
 
         // Delete the movie record from the database
         const deletedCast = await prisma.cast.delete({
